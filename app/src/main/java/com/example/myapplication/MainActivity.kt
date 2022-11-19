60f8daccd99fea99e3ed13e81dda8ad49391a397
HASAN AL-BAR <tfshan05@gmail.com>
2022-11-19 22:03:38 +0300

    Finished 100% of the bottom Navigation project

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView:BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val firstFragment = FirstFragment()
+        val secondFragment: Fragment = SeconedFragment()
+        val thirdFragment = ThirdFragment()
+
+        setCurrentFragment(firstFragment)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.miHome -> setCurrentFragment(firstFragment)
+                R.id.miMessages -> setCurrentFragment(secondFragment)
+                R.id.miProfile -> setCurrentFragment(thirdFragment)
+
+            }
+            true
+
+        }
     }
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
+
 }
---
+++ app/src/main/java/com/example/myapplication/SeconedFragment.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+import androidx.fragment.app.Fragment
+
+class SeconedFragment : Fragment(R.layout.fragment_seconed) {
+}
